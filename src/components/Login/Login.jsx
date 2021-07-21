b4603dfe4cc40061c10955d32fc4598db61c9a50
Criscamgel <caguio@ucompensar.edu.co>
2021-07-21 14:26:01 -0500

    Creación de todos los componentes, incorporación de mixins

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Login.scss';
+import BestPizza from '../../images/Login-Best-Pizza.png';
+
+export const Login = () => {
+    return (
+        <>
+
+            <div className="container">
+                <div className="content">
+                    <img className="center" src={BestPizza} alt="" />
+                    <form action="">
+                        
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
